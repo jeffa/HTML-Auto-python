d2f7a88b065fd6edde8df822f5f99d2b6e9cbc0d
jeffa <jeffa@unlocalhost.com>
2016-01-21 16:39:00 -0500

    corrected code/tests

--- HTML/Auto.py
+++ HTML/Auto.py
@@ -1,33 +1,38 @@
+from cgi import escape
+
 __version__='0.0.1'
 
 class Tag:
 
     def __init__( self, params={} ):
-        self.encodes = 1                 if 'encodes' in params else 0
+        self.encode  = 1                 if 'encode'  in params else 0
         self.sort    = 1                 if 'sort'    in params else 0
         self.level   = params['level']   if 'level'   in params else 0
-        self.encode  = params['encode']  if 'encode'  in params else ''
+        self.encodes = params['encodes'] if 'encodes' in params else ''
         self.indent  = params['indent']  if 'indent'  in params else ''
         self.newline = "\n"              if 'indent'  in params else ''
 
     def tag( self, params={} ):
 
+        tag   = params['tag']
+        cdata = params['cdata'] if 'cdata' in params else ''
+        attr  = params['attr']  if 'attr'  in params else {}
+
+        if not type( attr ) == 'Attr':
+            attr = Attr( attr, self.sort )
+
         # empty tag
         if 'cdata' not in params or not params['cdata']:
             return "<html />"
-
-        tag   = params['tag']
-        cdata = params['cdata']
-        attr  = params['attr'] if 'attr' in params else {}
 
         return '<' + tag + '>' + cdata + '</' + tag + '>'
 
 
 class Attr:
 
-    def __init__( self, params={} ):
-        self.hash   = hash
-        self.sorted = sorted
+    def __init__( self, params={}, sort=0 ):
+        self.params = params
+        self.sort   = sort
 
     def __str__( self ):
         str = ''
