9dca6bd302b69cc5ca3e4b589556b8d6ec1f070d
jeffa <jeffa@unlocalhost.com>
2016-01-28 12:57:26 -0500

    testings ...

--- t/02-tags.py
+++ t/02-tags.py
@@ -4,14 +4,32 @@ from HTML.Auto import Attr
 
 class TestTags(unittest.TestCase):
 
-    def test_tag(self):
+    def test_init(self):
         auto = Tag()
-        self.assertEqual( auto.tag( { 'tag':'html' } ), '<html />' )
-        self.assertEqual( auto.tag( { 'tag':'html', 'cdata':'' } ), '<html />' )
+        self.assertEqual( auto.encode, 0,       "no args encode correct" )
+        self.assertEqual( auto.encodes, '',     "no args encodes correct" )
+        self.assertEqual( auto.indent, '',      "no args indent correct" )
+        self.assertEqual( auto.level,  0,       "no args level correct" )
+        self.assertEqual( auto.sort, 0,         "no args sort correct" )
+        self.assertEqual( auto.newline, '',     "no args newline correct" )
 
-    def test_cdata(self):
+        auto = Tag({ 'encodes': '<>', 'indent': ' ', 'sort': 1, 'level': 2 })
+        #self.assertEqual( auto.encode, 1,       "encode set correct" )
+        self.assertEqual( auto.encodes, '<>',   "encodes set correct" )
+        self.assertEqual( auto.indent, ' ',    "indent set correct" )
+        self.assertEqual( auto.level, 2,        "sort set correct" )
+        self.assertEqual( auto.sort, 1,         "sort set correct" )
+        self.assertEqual( auto.newline, "\n",   "newline set correct" )
+
+    def test_empty(self):
         auto = Tag()
-        self.assertEqual( auto.tag( { 'tag':'foo', 'cdata':'bar' } ), '<foo>bar</foo>' )
+        self.assertEqual( auto.tag( { 'tag': 'html' } ), '<html />',                "no cdata correct" )
+        self.assertEqual( auto.tag( { 'tag': 'html', 'cdata': '' } ), '<html />',   "empty cdata correct" )
+        #self.assertEqual( auto.tag( { 'tag': 'html', 'cdata': ' ' } ), '<html />', "whitespace cdata correct" )
+
+    def test_nonempty(self):
+        auto = Tag()
+        self.assertEqual( auto.tag( { 'tag': 'p', 'cdata': '1' } ), '<p>1</p>',       "0 as cdata" )
 
 
 if __name__ == '__main__':
