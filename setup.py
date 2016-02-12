4cadef4ea0c4a9eb2ea1995267eaaad6728e9ae1
jeffa <jeffa@unlocalhost.com>
2016-02-12 12:59:04 -0500

    update meta data

--- setup.py
+++ setup.py
@@ -8,13 +8,16 @@ setup(
     author='Jeff Anderson',
     author_email='jeffa@cpan.org',
     url='https://github.com/jeffa/HTML-Auto-python',
-    license='MIT',
+    license='Artistic',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     classifiers=[
+        "Topic :: Text Processing :: Markup :: HTML",
         "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
+        "License :: OSI Approved :: Artistic License",
     ],
     long_description="""\
     Just another HTML tag generator.
