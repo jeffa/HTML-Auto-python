b752a5079f769b37fae99cbb1881d728b28bf285
jeffa <jeffa@unlocalhost.com>
2016-10-01 23:26:12 -0400

    more colgroup tests

--- t/04-tag-attrs.py
+++ t/04-tag-attrs.py
@@ -36,5 +36,28 @@ class TestTagAttrs(unittest.TestCase):
             'colgroup tag correct'
         )
 
+        self.assertEqual(
+            '<table><colgroup><col /><col /><col /></colgroup><colgroup><col /><col /><col /></colgroup><colgroup><col /><col /><col /></colgroup></table>',
+            auto.tag({ 'tag': 'table', 'cdata': [
+                {'tag': 'colgroup', 'attr': {}, 'cdata': [{'tag': 'col', 'attr': {}}, {'tag': 'col', 'attr': {}}, {'tag': 'col', 'attr': {}}] },
+                {'tag': 'colgroup', 'attr': {}, 'cdata': [{'tag': 'col', 'attr': {}}, {'tag': 'col', 'attr': {}}, {'tag': 'col', 'attr': {}}] },
+                {'tag': 'colgroup', 'attr': {}, 'cdata': [{'tag': 'col', 'attr': {}}, {'tag': 'col', 'attr': {}}, {'tag': 'col', 'attr': {}}] } ] }),
+            'colgroup tag correct'
+        )
+
+        self.assertEqual(
+            '<table><colgroup><col /><col /><col /></colgroup><colgroup><col /><col /><col /></colgroup><colgroup><col /><col /><col /></colgroup><tr><th>a</th><th>b</th><th>c</th></tr><tr><td>1</td><td>2</td><td>3</td></tr><tr><td>4</td><td>5</td><td>6</td></tr></table>',
+            auto.tag({ 'tag': 'table', 'cdata': [
+                {'tag': 'colgroup', 'attr': {}, 'cdata': [{'tag': 'col', 'attr': {}}, {'tag': 'col', 'attr': {}}, {'tag': 'col', 'attr': {}}]},
+                {'tag': 'colgroup', 'attr': {}, 'cdata': [{'tag': 'col', 'attr': {}}, {'tag': 'col', 'attr': {}}, {'tag': 'col', 'attr': {}}]},
+                {'tag': 'colgroup', 'attr': {}, 'cdata': [{'tag': 'col', 'attr': {}}, {'tag': 'col', 'attr': {}}, {'tag': 'col', 'attr': {}}]},
+                {'tag': 'tr', 'attr': {}, 'cdata': [{'tag': 'th', 'attr': {}, 'cdata': 'a'}, {'tag': 'th', 'attr': {}, 'cdata': 'b'}, {'tag': 'th', 'attr': {}, 'cdata': 'c'}]},
+                {'tag': 'tr', 'attr': {}, 'cdata': [{'tag': 'td', 'attr': {}, 'cdata': '1'}, {'tag': 'td', 'attr': {}, 'cdata': '2'}, {'tag': 'td', 'attr': {}, 'cdata': '3'}]},
+                {'tag': 'tr', 'attr': {}, 'cdata': [{'tag': 'td', 'attr': {}, 'cdata': '4'}, {'tag': 'td', 'attr': {}, 'cdata': '5'}, {'tag': 'td', 'attr': {}, 'cdata': '6'}]}
+            ] }),
+            'colgroup tag correct'
+        )
+
+
 if __name__ == '__main__':
     unittest.main()
